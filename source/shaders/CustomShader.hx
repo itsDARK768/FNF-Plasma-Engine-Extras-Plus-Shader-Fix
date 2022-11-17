abbeae60a07310a0124fe4e51fe0cc28d463e33d
Quansado deddingle <67988387+itsDARK768@users.noreply.github.com>
2022-11-16 23:03:15 -0600

    Added the function to turn on and off Shaders

--- source/shaders/CustomShader.hx
+++ source/shaders/CustomShader.hx
@@ -6,5 +6,13 @@ class CustomShader extends FlxRuntimeShader
 {
     public function new(?frag:String, ?vert:String = null, glslVersion:Int = 120) {
         super(frag, vert, glslVersion);
+        
+        if (Settings.get("Shaders"))
+            return;
+        else
+        {
+            trace("Shaders aren't enabled! Disabling module...");
+            return false;
+        }
     }
 }
