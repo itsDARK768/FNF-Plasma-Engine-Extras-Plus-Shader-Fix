440ee51763714ef365900b3feee6dd18b703fc1e
Quansado deddingle <67988387+itsDARK768@users.noreply.github.com>
2022-11-16 23:19:17 -0600

    Shader option removed

--- source/shaders/CustomShader.hx
+++ source/shaders/CustomShader.hx
@@ -7,12 +7,6 @@ class CustomShader extends FlxRuntimeShader
     public function new(?frag:String, ?vert:String = null, glslVersion:Int = 120) {
         super(frag, vert, glslVersion);
         
-        if (Settings.get("Shaders"))
-            return;
-        else
-        {
-            trace("Shaders aren't enabled! Disabling module...");
-            return false;
-        }
+        // the setting was removed, why the fuck would you remove fucking runtime shaders
     }
 }
